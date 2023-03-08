0517bd87adcb72676cf0cef7d4cd75921bc6f7ab
678435021 <amongusthem69@gmail.com>
2023-03-08 22:28:37 +0100

    Fix crash with empty config path

--- src/lib/helpers/configPath.ts
+++ src/lib/helpers/configPath.ts
@@ -3,22 +3,31 @@ import { mainPath, srcPath } from './mainPath.js';
 import { homedir } from 'os';
 import { join } from 'path';
 
-const dir: string = process.platform === 'win32' ? join(mainPath, 'config') : join(homedir(), '.config', 'mc-term');
+const defaultDir: string = process.platform === 'win32' ? join(mainPath, 'config') : join(homedir(), '.config', 'mc-term');
 
 /*
   Returns the file containing the config files' path's path
 */
-export const filePath: string = join(dir, '.configPath');
+export const filePath: string = join(defaultDir, '.configPath');
 
 /*
   Returns config files' path
 */
 function get (): string {
 	if (!existsSync(filePath)) {
-		mkdirSync(dir, { recursive: true });
-		writeFileSync(filePath, dir);
+		mkdirSync(defaultDir, { recursive: true });
+		writeFileSync(filePath, defaultDir);
+		return defaultDir;
 	}
-	return readFileSync(filePath, { encoding: 'utf8' });
+
+	const fileContent = readFileSync(filePath, { encoding: 'utf8' });
+
+	if (!fileContent) {
+		writeFileSync(filePath, defaultDir);
+		return defaultDir;
+	}
+
+	return fileContent;
 }
 
 export const configPath = get();
